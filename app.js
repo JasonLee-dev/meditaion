aac2dae25b9d5f1e4ec0c8eb1743b1e5984ca02e
JssonLee <jyk1045@naver.com>
2020-09-23 15:51:12 +0900

    add to website

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+// https://jasonlee-dev.github.io/meditaion/
 const app = () => {
   const song = document.querySelector(".song");
   const play = document.querySelector(".play");
   const outline = document.querySelector(".moving-outline circle");
   const video = document.querySelector(".vid-container video");
-
+  const defaultTime = 600;
   // sounds
   const sounds = document.querySelectorAll(".sound-picker button");
 
@@ -15,7 +16,7 @@ const app = () => {
   const outlineLength = outline.getTotalLength();
 
   // Duration
-  let fakeDuration = 600;
+  let fakeDuration = defaultTime;
   outline.style.strokeDasharray = outlineLength;
   outline.style.strokeDashoffset = outlineLength;
 
